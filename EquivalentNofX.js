9a35b3dc9269c092f4d451ec1b53beceb8bb65db
neticulis <neticule@gmail.com>
2017-08-10 19:53:03 -0400

    Update EquivalentNofX.js

--- EquivalentNofX.js
+++ EquivalentNofX.js
@@ -10,3 +10,22 @@ function get_equivExpFromNStreakOfX(streakLength=2,multiplier = 200){
 function get_equivSumFromNStreakOfX(streakLength=2,multiplier = 200){
   return (Math.round(((multiplier/100)*streakLength)*100))
 }
+// returns the equivalent exponential X for multpliers in array 
+// assuming we bet all our winnings and hit these in a streak we would win the equivalent of what is returned
+// ex 2x times 3x times 4x = 24x
+// ex 1.33x times 1.28x times 1.17x = 2x ... 100@1.33x->133@1.28x->170@1.17x = ~100 bit profit
+// 1.5 * 1.34 * 1.28 * 1.17 = 3.01x ... 100@1.5x->150@1.34->201@1.28x->257@1.17x = ~200 bit profit
+function get_equivExpFromMultipliers(arrayOfMultipliers = [200, 300, 400]){
+  if (typeof arrayOfMultipliers!='object'){
+    return false
+  }
+  return (arrayOfMultipliers.reduceRight(function(a,b,c){return Math.ceil((a/100)*b)}))
+}
+// returns the equivalent summed X for multpliers in array 
+// ex 2x + 3x + 4x = 9x
+function get_equivSumFromMultipliers(arrayOfMultipliers = [200, 300, 400]){
+  if (typeof arrayOfMultipliers!='object'){
+    return false
+  }
+  return (arrayOfMultipliers.reduceRight(function(a,b,c){return Math.ceil(a+b)}))
+}
